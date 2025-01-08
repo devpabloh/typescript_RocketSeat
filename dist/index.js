5f53c5a68a1c2ac8284426f49c38d285e5425568
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-08 16:13:33 -0300

    Criando repositório para matéria de typescript no curso da rocketseat

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// se quiser ativar o watch para compilar automaticamente quando houver alguma mudança, basta escrever no terminal npx tsc --watch
+const mensagem = "Olá, mundo!";
+console.log(mensagem);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// se quiser ativar o watch para compilar automaticamente quando houver alguma mudança, basta escrever no terminal npx tsc --watch
+
+const mensagem: string = "Olá, mundo!"
+console.log(mensagem)
