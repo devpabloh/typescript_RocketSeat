7081b3867ed2f0c0f8a9167bed9bcefdd011cf1e
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-08 16:28:57 -0300

    att type

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,12 @@
 "use strict";
 // se quiser ativar o watch para compilar automaticamente quando houver alguma mudança, basta escrever no terminal npx tsc --watch
 const mensagem = "Olá, mundo!";
-console.log(mensagem);
+// Consigo mudar?
+//mensagem = "typescript"
+// Consigo executar
+mensagem();
+const user = {
+    name: "Pablo Henrique Ferreira de França",
+    email: "pablohenriqueadm94@gmail.com"
+};
+console.log(user.avatar);
